493ef43504a3398f90a7f48f43979e814f00cfb7
loofeht <phu832001@gmail.com>
2021-10-19 20:00:46 +0700

    còn thiếu list[0] tức thống tin người dùng

--- index.php
+++ index.php
@@ -1,58 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include './reuse/header.php' ?>
+<div class="wrapper">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Stt</th>
+                <th scope="col">Số báo danh</th>
+                <th scope="col">Mã bài thi</th>
+                <th scope="col">Ngày thi</th>
+                <th scope="col">Giờ bài nộp</th>
+                <th scope="col">Điểm thi</th>
+                <th scope="col">Tệp kết quả</th>
+                <th scope="col">Kí tên</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $file = scandir('./result');
+            for ($i = 2; $i < count($file); $i++) {
+                $items = explode('_', $file[$i]);
+                $label = explode('.', $items[2])[0];
+                $date = explode('.', $items[4])[0];
+                $time = explode('.', $items[5])[0];
+                $mark = explode('.', $items[6])[0];
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Document</title>
-</head>
+                echo "<tr>";
+                echo "<td>" . ($i - 1) . "</td>";
+                echo "<td>chy01024</td>";
+                echo "<td>$label</td>";
+                echo "<td>$date</td>";
+                echo "<td>$time</td>";
+                echo "<td>$mark</td>";
+                echo '<td><a href="view.php?file=result/' . $file[$i] . '">' . $file[$i] . '</a></td>';
+                echo "<td></td>";
+                echo "</tr>";
+            }
+            ?>
 
-<body>
+        </tbody>
+    </table>
+</div>
 
-    <div class="wrapper">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Stt</th>
-                    <th scope="col">Số báo danh</th>
-                    <th scope="col">Mã bài thi</th>
-                    <th scope="col">Ngày thi</th>
-                    <th scope="col">Giờ bài nộp</th>
-                    <th scope="col">Điểm thi</th>
-                    <th scope="col">Tệp kết quả</th>
-                    <th scope="col">Kí tên</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $file = scandir('./result');
-                for ($i = 2; $i < count($file); $i++) {
-                    $items = explode('_', $file[$i]);
-                    $label = explode('.', $items[2])[0];
-                    $date = explode('.', $items[4])[0];
-                    $time = explode('.', $items[5])[0];
-                    $mark = explode('.', $items[6])[0];
-
-                    echo "<tr>";
-                    echo "<td>" . ($i - 1) . "</td>";
-                    echo "<td>chy01024</td>";
-                    echo "<td>$label</td>";
-                    echo "<td>$date</td>";
-                    echo "<td>$time</td>";
-                    echo "<td>$mark</td>";
-                    echo '<td><a href="view.php?file=result/' . $file[$i] . '">' . $file[$i] . '</a></td>';
-                    echo "<td></td>";
-                    echo "</tr>";
-                }
-                ?>
-
-            </tbody>
-        </table>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php include './reuse/footer.php' ?>
--- view.php
+++ view.php
@@ -1,39 +1,51 @@
 <?php
+include './reuse/header.php';
 $filename = $_GET['file'];
-
 $fp = fopen($filename, "r"); //mở file ở chế độ đọc
 $contents = fread($fp, filesize($filename));
-$items = explode("\n\n", $contents);
-$item = explode("\n", $items[9]);
-$mark = explode(":", $item[1]);
-echo $mark[1];
-// echo $items[1]; //in nội dung của file ra màn hình
+
+//! demo
+// $list = explode("\n\n", $contents); //? cắt chuỗi khi có 2 lần enter liên tục trả về list
+// $items = explode("\n", $list[1]); //? cắt lấy từng dòng của list ở vị trí 1 
+// echo $items[1] . "<br>"; //? lấy ra item ở vị trí số 1 tức là dòng award points
+// echo $items[3] . "<br>";
+
+// $mark = explode(":", $items[1]); //? cắt award points phân cách bằng :
+// $response = explode(":", $items[3]);
+// echo $mark[1] . "<br>"; //? lấy ra chuỗi sau :
+// echo $response[1] . "<br>"; //? lấy ra chuỗi sau :
 ?>
 
-<table class="table">
+<table class="table table-bordered ">
     <thead>
-        <tr>
-            <th scope="col">Stt</th>
-            <th scope="col">Nội dung câu hỏi</th>
-            <th scope="col">Điểm đạt</th>
-            <th scope="col">Đáp án đã chọn</th>
+        <tr class="text-center table-primary">
+            <th class="col" scope="col">Stt</th>
+            <th class="col-6" scope="col">Nội dung câu hỏi</th>
+            <th class="col-1" scope="col">Điểm đạt</th>
+            <th class="col-6" scope="col">Đáp án đã chọn</th>
         </tr>
     </thead>
 
     <tbody>
         <?php
-        $items = explode("\n\n", $contents);
-        for ($i = 1; $i < count($items) - 1; $i++) {
-            $item = explode("\n", $items[$i]);
-            // $mark = explode(":", $item[$i]);
+
+        $list = explode("\n\n", $contents);
+        for ($i = 1; $i < count($list) - 1; $i++) {
+            $items = explode("\n", $list[$i]); //? cắt từng dòng của tất cả các list
+
+            $mark = explode(":", $items[1]); //? cắt awarded points làm 2
+
+            $response = explode(":", $items[3]); //? cắt đáp án làm 2
 
             echo "<tr>";
             echo "<td>$i</td>";
-            echo "<td>$item[0]</td>";
-            echo "<td>$item[1]</td>";
-            echo "<td>$item[3]</td>";
+            echo "<td>$items[0]</td>";
+            echo "<td>$mark[1]</td>";
+            echo "<td>$response[1]</td>";
             echo "</tr>";
         }
         ?>
 
     </tbody>
+
+    <?php include './reuse/header.php'; ?>
